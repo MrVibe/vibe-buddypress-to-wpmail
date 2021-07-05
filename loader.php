26b2b1d0d4c1648ee9506ad994eaeabf6b9ec330
Ripul MacbookPro <kumar.ripul@gmail.com>
2021-07-05 12:45:11 +0530

    init

---
+++ loader.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Vibe BuddyPress to WP Mail fix
+Plugin URI: https://www.Vibethemes.com
+Description: Send BuddyPress emails via WP Mail
+Version: 1.0
+Author: VibeThemes
+Author URI: http://www.vibethemes.com
+License: GPLv3
+Text Domain: vibe-bbb
+Domain Path: /languages/
+*/
+
